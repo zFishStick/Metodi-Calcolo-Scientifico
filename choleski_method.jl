8a8abc58fe41f2685f2ff941381b585694d59062
Simone Moretti <s.moretti29@campus.unimib.it>
2026-03-31 17:07:05 +0200

    "julia"

--- choleski_method.jl
+++ choleski_method.jl
@@ -1,41 +1,43 @@
 # Decomposizione di Cholesky in Julia
 using LinearAlgebra
+using SparseArrays, MAT
 
 
-function cholesky_decomposition(A::Matrix{Float64}, b::Vector{Float64} = [])
+function cholesky_decomposition(A::Matrix{Float64}, b::Vector{Float64} = Float64[])
 
-    # Controllo se la matrice è simmetrica
-    if !(issymmetric(A))
-        error("La matrice deve essere simmetrica.")
+    folder = "matrici\\"
+    files = readdir(folder)
+    matrici = []
+    for f in files
+        data = matread(joinpath(folder, f))
+        push!(matrici, data["Problem"]["A"])
     end
 
-    # Controllo se la matrice è definita positiva, inutile perchè la sua descrizione è
-    # "Test whether a matrix is positive definite (and Hermitian) by trying to perform a Cholesky factorization of A."
-    # if !(isposdef(A))
-    #     error("La matrice deve essere definita positiva.")
-    # end
+    for A in matrici
+        # Controllo se la matrice è simmetrica
+        if !(issymmetric(A))
+            error("La matrice deve essere simmetrica.")
+        end
 
-    # Soluzione esatta xe = [1,1,1,1...]
-    xe = ones(size(A, 1))
+        # Controllo se la matrice è definita positiva, inutile perchè la sua descrizione è
+        # "Test whether a matrix is positive definite (and Hermitian) by trying to perform a Cholesky factorization of A."
+        # if !(isposdef(A))
+        #     error("La matrice deve essere definita positiva.")
+        # end
 
-    if isempty(b)
+        # Soluzione esatta xe = [1,1,1,1...]
+        xe = ones(size(A, 1))
+        
         b = A * xe
+       
+        x = A \ b
+
+        println("Soluzione: ", x)
     end
-
-    f = cholesky(A)
-
-    println("Fattorizzazione di Cholesky: ", f)
-
-    x = f \ b
-
-    println("Soluzione: ", x)
 
     return x
-        
 end
 
 A = [4.0 10.0 8.0; 10.0 26.0 26.0; 8.0 26.0 61.0]
-
-b = [44.0, 128.0, 214.0]
 
 L = cholesky_decomposition(A, b) # La funzione funzia
